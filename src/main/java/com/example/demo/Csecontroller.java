83a9cdad9bff73a25ebda8b4209611aad5d480a3
Swethamadda0710 <maddaswetha9@gmail.com>
2026-02-26 11:51:44 +0530

    commit

--- src/main/java/com/example/demo/Csecontroller.java
+++ src/main/java/com/example/demo/Csecontroller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Csecontroller {
 	@GetMapping("/adityacse")
 	public String rocks() {
-		return "Welcome to aditya cse";
+		return "Welcome to cse 3rd year";
 	}
 	
 	@GetMapping("/adityait")
